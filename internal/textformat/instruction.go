c574a207e94451380bde0b6ff30d4e0d40638072
Eli Bendersky <eliben@gmail.com>
2026-03-06 07:35:53 -0800

    Remove unused instruction enums in textformat

--- internal/textformat/instruction.go
+++ internal/textformat/instruction.go
@@ -43,26 +43,3 @@ type KeywordOperand struct {
 }
 
 func (*KeywordOperand) isOperand() {}
-
-// TODO:
-// (control instructions are special)
-// For each plain instruction, we need its string representation (to read from
-// the input), and some description of its parameters (arity and their types)
-type instrName int
-
-const (
-	NONE instrName = iota
-	I32Load
-	I64Load
-	F32Load
-	F64Load
-	// TODO more memory instructions
-
-	I32Const
-	I64Const
-	F32Const
-	F64Const
-
-	I32Add
-	I32Sub
-)
